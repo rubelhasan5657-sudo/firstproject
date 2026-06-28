a968ef70d40b4d7d9f0f5e89f1f0d64ed62e4d6e
rubelhasan5657-sudo <rubelhasan5657@gmail.com>
2026-06-28 17:35:04 +0600

    add f string update

--- datatest.py
+++ datatest.py
@@ -12,3 +12,12 @@ s2 = "programming"
 
 # Use format() method to create a new string that includes both variables
 res2 = "{} is a popular language for {}".format(s1, s2)
+
+
+s1 = "Python"
+s2 = "programming"
+
+# Use an f-string to create a formatted string
+# that includes both variables
+res3 = f"{s1} is a popular language for {s2}"
+
